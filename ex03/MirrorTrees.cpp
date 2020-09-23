1de3d6eb7df1bba26c3a0bd71e372d09fb56528d
Rozemelnudre <roze.melnudre@gmail.com>
2020-09-23 13:13:34 +0300

    initial structure

--- ex03/MirrorTrees.cpp
+++ ex03/MirrorTrees.cpp
@@ -7,5 +7,36 @@
 
 
 using namespace std;
+using namespace ds_course;
+
+Node::Node(int vv){
+    val = vv;
+}
+
+
+int Node::getVal(){
+    return this->val;
+}
+
+vector<int> Node::getChildren(){
+    vector<int> returnVector;
+    for (int i = 0; i < (this->children).size(); i++){
+        returnVector.push_back(this->children[i].val);
+    }
+
+    return returnVector;
+}
+
+void Node::addChild(Node child){
+    this->children.push_back(child);
+}
+
+void Node::reflect(){
+
+}
+
+void Node::printPreorder(){
+
+}
 
 
--- ex03/MirrorTreesMain.cpp
+++ ex03/MirrorTreesMain.cpp
@@ -4,55 +4,15 @@
 #include <sstream>
 #include <ctype.h>
 #include <iomanip>
+#include "MirrorTrees.h"
 
 using namespace std;
 using namespace ds_course;
 
-int main()
-
-{    
-    int count;
-    cin >> count;
-    Student stdts[count] = {};
-    string temp;
-    string tempage = "";
-    string tempheight = "";
-    double height;
-    int age;
-    int studentcounter = 0;
-    long long int delimiter = 0;
-    
-
-    //cout << '\'' << mode << '\'' << endl;
-    cin.ignore(10000,'\n');
-
-    string inputLine;
-    while (getline(cin, inputLine)) {
-        istringstream sstr(inputLine);
-        if (sstr.peek() == '#') {
-            continue;
-        }
-        else {
-           sstr >> age;
-           sstr >> height;
-
-            Student newbie;
-            newbie.age = age;
-            newbie.height = height;
-
-            stdts[studentcounter] = newbie;
-            studentcounter++;
-            
-           
-        }
-    }
-
-
-
-   
-    cout << maxStudent.age << " " << fixed << setprecision(5) << maxStudent.height << endl;
-    cout << minStudent.age << " " << fixed << setprecision(5) << minStudent.height << endl;
-    return 0;
+int main(){  
+    cout << "hello" << endl;
+    Node n1(13);
+    cout<< "node has val" << n1.getVal() << endl;  
 }
 
 
