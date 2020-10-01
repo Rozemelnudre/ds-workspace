b31bb49d8abc8e5d97b5d6cc8b0cab3bc508825e
Rozemelnudre <roze.melnudre@gmail.com>
2020-10-01 11:54:23 +0300

    initial structure

--- ex04/Ratio.h
+++ ex04/Ratio.h
@@ -1,79 +1,75 @@
-#ifndef RATIO_H
-#define RATIO_H
+#ifndef DS_RATIO_H
+#define DS_RATIO_H
 
 #include <iostream>
 
-namespace ds_course{
+// using namespace ... - DON'T DO THIS
 
-    class Ratio{
-        private:
-          int numerator;
-          int denominator; 
-          int gcd(int a , int b);
-        public:
-            Ratio operator+(Ratio& rhs);
-            Ratio operator-(Ratio& rhs);
-            Ratio operator*(Ratio& rhs);
-            Ratio operator/(Ratio& rhs);
-            Ratio(int nn = 0, int dd = 1);
+// Here is stuff that is in header files
+// Declarations only
+namespace ds_course
+{
+    class Ratio
+    {
+    private:
+        int num;
+        int den;
+        int gcd(int a, int b);
 
-        friend std::ostream& operator<< (std::ostream& oStream, const Ratio rat){
-        oStream << "[" << rat.numerator << "/" << rat.denominator << "]";
-        return oStream;
-    }
+    public:
+        Ratio(int nn = 0, int dd = 1);
+        Ratio operator+(const Ratio &rhs);
+        bool operator==(const Ratio &rhs);
 
+        friend std::ostream &operator<<(std::ostream &oStream, const Ratio &rat)
+        {
+            oStream << "[" << rat.num << "/" << rat.den << "]";
+            return oStream;
+        }
+
+        friend std::istream &operator>>(std::istream &input, ds_course::Ratio &rat)
+        {
+            input >> rat.num;
+            input.ignore(256, '/'); // ignore slash
+            input >> rat.den;
+            return input;
+        }
     };
 
+} // namespace ds_course
 
-
-
-
-
-int Ratio::gcd(int a, int b){
-    return 0;
+// Here comes stuff that you would normally put in a CPP file
+// THe DEFINITIONS of your member functions and friends....
+int ds_course::Ratio::gcd(int a, int b)
+{
+    return 1; // stubs,
 }
 
-Ratio::Ratio(int nn = 0, int dd = 1): numerator(nn), denominator(dd){
+ds_course::Ratio::Ratio(int nn, int dd) : num(nn), den(dd)
+{
     int g = gcd(nn, dd);
-    numerator = nn / g;
-    denominator = dd / g;
-    if(denominator < 0){
-        numerator = -numerator;
-        denominator = -denominator;
+    num = num / g;
+    den = den / g;
+    if (den < 0)
+    {
+        num = -num;
+        den = -den;
     }
-
 }
 
-
-
-Ratio Ratio::operator+(Ratio& rhs){
-    int upside = numerator * rhs.denominator + denominator * rhs.numerator;
-    int downside = denominator * rhs.denominator;
-    Ratio result(upside, downside);
+// result of r1 + r2 (Return type)
+ds_course::Ratio ds_course::Ratio::operator+(const ds_course::Ratio &rhs)
+{
+    int upside = num * rhs.den + den * rhs.num;
+    int downside = den * rhs.den;
+    ds_course::Ratio result(upside, downside);
     return result;
 }
 
-Ratio Ratio::operator-(Ratio& rhs){
-    int upside = numerator * rhs.denominator - denominator * rhs.numerator;
-    int downside = denominator * rhs.denominator;
-    Ratio result(upside, downside);
-    return result;
+bool ds_course::Ratio::operator==(const ds_course::Ratio &r) {    
+    return (num * r.den == den * r.num);
 }
 
-Ratio Ratio::operator*(Ratio& rhs){
-    int upside = numerator * rhs.numerator;
-    int downside = denominator * rhs.denominator;
-    Ratio result(upside, downside);
-    return result;
-}
 
-Ratio Ratio::operator/(Ratio& rhs){
-    int upside = numerator * rhs.denominator;
-    int downside = denominator * rhs.denominator;
-    Ratio result(upside, downside);
-    return result;
-}
-
-}
 
 #endif
