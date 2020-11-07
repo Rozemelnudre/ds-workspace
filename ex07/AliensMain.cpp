62f247c080989d16e420b6511880368070fe3b23
Rozemelnudre <roze.melnudre@gmail.com>
2020-11-07 23:20:48 +0200

    error handling

--- ex07/AliensMain.cpp
+++ ex07/AliensMain.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
-#include <vector> 
-#include <string>
-#include <map>
 #include <sstream>
 #include "Node.h"
 
 using namespace ds_course;
 using namespace std;
+
+int traversal[10000];
+int trcount = 0;
+
+void inOrderTraversal(Node* node);
+
 
 int main(){
 
@@ -14,67 +17,225 @@ int main(){
     string command;
     int data;
     int parentval;
-    vector<int> traversal;
-    map<int, Node*> existingNodes;
-    Node* current;
+    //Node* current;
     Node* root;
     Node* newNode;
+    Node* nodesAdded[10000];
+    int counter = 0;
+    Node* forest[10000];
+    int frstcnt = 0;
     
     while(getline(cin, line)){
         istringstream inputLine(line);
+        
 
         inputLine >> command;
+        //cout << "right now doing " << command << endl;
 
         if(command == "T"){
             inputLine >> data;
-            current = new Node(data);
-            root = current;
-            existingNodes.insert({data, current}); 
+            //cout << "Adding ancestor with val " << data << endl;
+            bool isUniqueRoot = true;
+            for(int i = 0; i < counter; i++){
+                if(nodesAdded[i]->value == data){
+                    isUniqueRoot = false;
+                    break;
+                }
+            }
+            if(!isUniqueRoot){
+                cout << "error6 \n";
+                continue;
+            }
+            newNode = new Node(data);
+            newNode->isRoot = true;
+            forest[frstcnt] = newNode;
+            frstcnt++;
+            nodesAdded[counter] = newNode;
+            counter ++;
 
         }else if(command == "L"){
+
+            bool leftChildExists = false;
+            bool parentHasChild = false;
+            bool leftParentFound = false;
             
             inputLine >> parentval;
             inputLine >> data;
+            //cout << "trying to add "<< parentval << " to " << data << endl;
 
-            if(current->value == parentval){
-                newNode = new Node(data);
-                current->addLeftChild(newNode);
-                current = newNode;
-                existingNodes.insert({data,current});
-            }else{
-                if(existingNodes.find(parentval) != existingNodes.end()){
-                    newNode = new Node(data);
-                    existingNodes.at(parentval)->addLeftChild(newNode);
-                    current = newNode;
-                    existingNodes.insert({data,current});
+            if(parentval == data){
+                cout << "error1 \n";
+                continue;
+            }
+
+            for(int m = 0; m < counter; m++){
+                if(nodesAdded[m]->value == data){
+                    leftChildExists = true;
+                    break;
                 }
             }
+
+
+
+            for(int i = 0; i < counter; i++){
+
+                if(nodesAdded[i]->value == parentval){
+
+                    if(nodesAdded[i]->leftChild != NULL){
+                        parentHasChild = true;
+                        break;
+                    }else{
+                        leftParentFound = true;
+                    }
+                   
+
+                    if(!leftChildExists && !parentHasChild){
+                        newNode = new Node(data);
+                        if(nodesAdded[i]->isRoot){
+                            newNode->root = nodesAdded[i];
+                        }else{
+                            newNode->root = nodesAdded[i]->root;
+                        }
+                       nodesAdded[i]->addLeftChild(newNode); 
+                       nodesAdded[counter] = newNode;
+                       counter ++;
+                    }
+                    
+
+                    nodesAdded[counter] = newNode;
+                    counter ++;
+                    break;
+                }
+            }
+            if(leftParentFound == false){
+                cout << "error2 \n";
+            }else if(leftChildExists){
+                cout << "error3 \n";
+            }else if(parentHasChild){
+                cout << "error4 \n";
+            }
+           
+
         }else if(command == "R"){
+            bool rightChildExists = false;
+            bool rParentHasChild = false;
+            bool rightParentFound = false;
 
             inputLine >> parentval;
             inputLine >> data;
+            //cout << "trying to add "<< data << " to " << parentval << endl;
 
-            if(current->value == parentval){
-                newNode = new Node(data);
-                current->addRightChild(newNode);
-                current = newNode;
-                existingNodes.insert({data,current});
-            }else{
-                if(existingNodes.find(parentval) != existingNodes.end()){
-                    newNode = new Node(data);
-                    existingNodes.at(parentval)->addRightChild(newNode);
-                    current = newNode;
-                    existingNodes.insert({data,current});
+            if(parentval == data){
+                cout << "error1 \n";
+                continue;
+            }
+
+            for(int m = 0; m < counter; m++){
+                if(nodesAdded[m]->value == data){
+                    rightChildExists = true;
+                    break;
                 }
             }
 
-        }else if(command == "?"){
 
+            for(int i = 0; i < counter; i++){
+                if(nodesAdded[i]->value == parentval){
+
+                    if(nodesAdded[i]->rightChild != NULL){
+                        //cout << "rightChild full" << endl;
+                        rParentHasChild = true;
+                        break;
+                    }else{
+                        //cout << "found right parent" << endl;
+                        rightParentFound = true;
+                    }
+
+                    if(!rightChildExists && !rParentHasChild){
+                        newNode = new Node(data);
+                        if(nodesAdded[i]->isRoot){
+                            newNode->root = nodesAdded[i];
+                        }else{
+                            newNode->root = nodesAdded[i]->root;
+                        }
+                        nodesAdded[i]->addRightChild(newNode);
+
+                        nodesAdded[counter] = newNode;
+                        counter ++;
+                        break;
+                    }
+                }
+            }
+            if(!rightParentFound){
+              cout << "error2 \n";  
+            }else if(rParentHasChild){
+                cout << "error3 \n";
+            }else if(rightChildExists){
+                cout << "error5";
+            }
+            
+
+        }else if(command == "?"){
+            inputLine >>data;
+            //cout << "searching for " << data << endl;
+            bool found = false;
+            trcount = 0;
+            for(int i = 0; i < counter; i++){
+                if(nodesAdded[i]->value == data){
+                    if(nodesAdded[i]->isRoot){
+                        inOrderTraversal(nodesAdded[i]);
+                    }else{
+                       inOrderTraversal(nodesAdded[i]->root); 
+                    }
+                    found = true;
+                    break;
+                    
+                }
+            }
+            if(!found){
+                cout << "error0 \n";
+                continue;
+            }
+            if(trcount == 1){
+                if(traversal[0] == data){
+                   cout << "0 0 \n" ; 
+                }else{
+                    /*throw some error*/
+                }
+                
+            }else{
+               for (int i = 0; i < trcount; i++){
+                    if(traversal[i] == data){
+
+                        if(i == 0){
+                            cout << "0 ";
+                        }else{
+                            cout << traversal[i-1] << " ";
+                        }
+
+                        if(i == trcount -1){
+                            cout << "0 \n";
+                        }else{
+                            cout << traversal[i+1] << "\n";
+                        }
+                    }
+                }  
+            }
+           
         }
 
     }
 
-
-
     return 0;
+}
+
+void inOrderTraversal(Node* root){
+    if(root->leftChild != NULL){
+        inOrderTraversal(root->leftChild);
+    }
+    traversal[trcount] = root->value;
+    trcount ++;
+    if(root->rightChild != NULL){
+        inOrderTraversal(root->rightChild);
+    }
+
 }
--- ex07/Node.h
+++ ex07/Node.h
@@ -11,10 +11,14 @@ namespace ds_course{
             int value;
             Node* leftChild;
             Node* rightChild;
+            Node* root;
+            bool isRoot;
             Node(int vval){
                 value = vval;
                 leftChild = NULL;
                 rightChild = NULL;
+                root = NULL;
+                isRoot = false;
             }
             ~Node();
             void addLeftChild(Node* child){
@@ -25,35 +29,6 @@ namespace ds_course{
             }
 
     };
-
-using namespace std;
-
-    vector<int> inOrderTraversal(Node* root){
-        vector<int> alienData;
-        stack<Node*> stc;
-        Node* current = root;
-        while(current != NULL || stc.empty() == false){
-            while (current !=  NULL) {
-            
-                /* place pointer to a tree node on 
-                the stack before traversing 
-                the node's left subtree */
-                stc.push(current); 
-                current = current->leftChild; 
-            } 
-    
-            /* Current must be NULL at this point */
-            current = stc.top(); 
-            stc.pop(); 
-    
-            alienData.push_back(current->value);
-    
-            /* we have visited the node and its 
-            left subtree.  Now, it's right 
-            subtree's turn */
-            current = current->rightChild; 
-            }
-    }
 
 }
 
